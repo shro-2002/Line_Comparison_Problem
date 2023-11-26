ca0aa285afcca97ab7686f38f7f7810ef7eef4ab
shro-2002 <96790181+shro-2002@users.noreply.github.com>
2023-11-26 13:16:41 +0530

    Added UC-1

--- LineComparison.java
+++ LineComparison.java
@@ -38,8 +38,25 @@ public class LineComparison {
 		System.out.println("Enter y2 co-ordinate of line 2: ");
 		int y4 = sc.nextInt();
 
+		// Length of lines
+		System.out.println("Length of line 1 is: " + lengthOfLine(x1, y1, x2, y2));
+		System.out.println("Length of line 2 is: " + lengthOfLine(x3, y3, x4, y4));
 
+	}
 
+	/*
+	 * @params: int
+	 * 
+	 * @return: length of line
+	 * 
+	 * @Description: calculate length of line
+	 */
+
+	public static double lengthOfLine(int x1, int y1, int x2, int y2) {
+		double x = Math.pow(x2 - x1, 2);
+		double y = Math.pow(y2 - y1, 2);
+		double length = Math.sqrt(x + y);
+		return length;
 	}
 
 }
