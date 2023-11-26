e195f31746219fa5d4f370752587337628edad3f
shro-2002 <96790181+shro-2002@users.noreply.github.com>
2023-11-26 13:19:38 +0530

    Added UC-2

--- LineComparison.java
+++ LineComparison.java
@@ -2,10 +2,6 @@ package com.bridgelabs.Welcome;
 
 import java.util.Scanner;
 
-import com.bridgelabs.usecase1.LengthLine;
-import com.bridgelabs.usecase2.EqualLines;
-import com.bridgelabs.usecase3.CompareLines;
-
 public class LineComparison {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
@@ -42,6 +38,9 @@ public class LineComparison {
 		System.out.println("Length of line 1 is: " + lengthOfLine(x1, y1, x2, y2));
 		System.out.println("Length of line 2 is: " + lengthOfLine(x3, y3, x4, y4));
 
+		// Equality of lines
+		equalityOfLines(x1, y1, x2, y2, x3, y3, x4, y4);
+
 	}
 
 	/*
@@ -57,6 +56,31 @@ public class LineComparison {
 		double y = Math.pow(y2 - y1, 2);
 		double length = Math.sqrt(x + y);
 		return length;
+	}
+
+	/*
+	 * @params: x1, y1, x2, y2, x3, y3, x4, y4
+	 * 
+	 * @return: boolean value
+	 * 
+	 * @Description: check equality of two lines
+	 */
+
+	public static boolean equalityOfLines(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
+
+		double lengthOfLine1 = lengthOfLine(x1, y1, x2, y2);
+		double lengthOfLine2 = lengthOfLine(x3, y3, x4, y4);
+
+		String str1 = String.valueOf(lengthOfLine1);
+		String str2 = String.valueOf(lengthOfLine2);
+
+		if (str1.equals(str2)) {
+			System.out.println("Line 1 is equal to Line 2");
+			return true;
+		} else {
+			System.out.println("Line 1 is not equal to Line 2");
+			return false;
+		}
 	}
 
 }
